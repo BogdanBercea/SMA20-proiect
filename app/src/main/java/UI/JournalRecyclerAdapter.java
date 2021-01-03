48eda01293fd60a1748e81b007c67b37d332775b
BerceaBogdan <bogdanbercea6@gmail.com>
2021-01-03 10:16:18 +0200

    Final Project State

---
+++ app/src/main/java/UI/JournalRecyclerAdapter.java
@@ -0,0 +1,104 @@
+package UI;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.journalapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import model.Journal;
+
+public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecyclerAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Journal> journalList;
+
+    public JournalRecyclerAdapter(Context context, List<Journal> journalList) {
+        this.context = context;
+        this.journalList = journalList;
+    }
+
+    @NonNull
+    @Override
+    public JournalRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(context)
+               .inflate(R.layout.journal_row, parent, false);
+
+       return new ViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull JournalRecyclerAdapter.ViewHolder holder, int position) {
+        Journal journal = journalList.get(position);
+        String imageUrl;
+
+        holder.title.setText(journal.getTitle());
+        holder.thoughts.setText(journal.getThought());
+        holder.name.setText(journal.getUserName());
+        imageUrl = journal.getImageUrl();
+
+        //Inspired by: https://medium.com/@shaktisinh/time-a-go-in-android-8bad8b171f87
+        String timeAgo = (String) DateUtils.getRelativeTimeSpanString(journal
+                .getTimeAdded()
+                .getSeconds() * 1000); //because we gat milliseconds and we need seconds
+        holder.dateAdded.setText(timeAgo);
+
+
+        // Inspired by: https://square.github.io/picasso/
+        //Use Picasso library to download and show image
+        Picasso.get()
+                .load(imageUrl)
+                .placeholder(R.drawable.image_three)
+                .fit()
+                .into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return journalList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView thoughts;
+        public TextView dateAdded;
+        public TextView name;
+
+        public ImageView image;
+        public ImageButton shareButton;
+
+        String userId;
+        String username;
+
+
+        public ViewHolder(@NonNull View itemView, Context ctx) {
+            super(itemView);
+            context = ctx;
+
+            title = (TextView) itemView.findViewById(R.id.journal_title_list);
+            thoughts = (TextView) itemView.findViewById(R.id.journal_thought_list);
+            dateAdded = (TextView) itemView.findViewById(R.id.journal_timestamp_list);
+            name = (TextView) itemView.findViewById(R.id.journal_row_username);
+            image = (ImageView) itemView.findViewById(R.id.journal_image_list);
+            shareButton = (ImageButton) itemView.findViewById(R.id.journal_row_share_button);
+
+            shareButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
+}
